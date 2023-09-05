44b9911df163778bba8a21ca017ecbc9d2cd25fe
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-05 10:40:32 +0500

    Create labtask2.c

---
+++ lab/03/labtask2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+      int l;
+      int w;
+      int h; 
+          printf("enter length");
+          scanf("%d",&l);
+          printf("enter width");
+          scanf(" %d",&w);
+          printf("enter hight");
+          scanf(" %d",&h);
+          int volum= l*h*w;
+          printf("volum is:%d",volum);
+return 1;
+}//end main
