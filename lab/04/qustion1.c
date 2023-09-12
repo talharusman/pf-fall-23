5c10e0b768604f22a24c1e1a51abb893394f47db
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-12 10:52:00 +0500

    Update qustion1.c

--- lab/04/qustion1.c
+++ lab/04/qustion1.c
@@ -1,6 +1,6 @@
 /* programmer: talha rusman
 * Date: 12/9/2023
-* Related file: this file of qustion 1
+* Related file:  programe of qustion 1
 */
 //--include file--//
 #include<stdio.h>
@@ -8,7 +8,7 @@
 //--Global Variables--//
 int main(){
 	int n;
-	printf("enter the integer n is gater then 0");
+	printf("enter  n is gater then 0");
 	scanf("%d",&n);
 	for(int i=0; i<=n ; i++){
 		for( int j=0 ; j<i; j++){ 
