ddf8accf06387c48578245c565db6444df8c0a2a
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-12 20:02:19 +0500

    Update qustion1.c

--- lab/04/qustion1.c
+++ lab/04/qustion1.c
@@ -1,20 +1,24 @@
 /* programmer: talha rusman
 * Date: 12/9/2023
-* Related file:  programe of qustion 1
+* Related file:  Print the pattern for any number of n. Below is an example for n=4.
+*
+**
+***
+****
 */
 //--include file--//
 #include<stdio.h>
 #include<stdlib.h>
 //--Global Variables--//
 int main(){
-	int n;
+	int n;//input from user
 	printf("enter  n is gater then 0");
 	scanf("%d",&n);
 	for(int i=0; i<=n ; i++){
 		for( int j=0 ; j<i; j++){ 
         		printf("*");
-      		}
+      		}//end of j<i
 		printf("\n");
-	}
+	}//end of i<=n
 	return 0;
 }//end main
