3130c2c03b3fc84288538f7b1cfaaba4c6f27348
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-12 19:51:04 +0500

    Update qustion3.c

--- lab/04/qustion3.c
+++ lab/04/qustion3.c
@@ -1,20 +1,27 @@
-/* programmer: talha rusman
-* Date: 12/9/2023
-* Related file: file of qustiom 3
+/* programer:talha rusman 
+* Date: 1292023
+* Related file: this program that asks user for a number n, then asks for n more
+* integers and prints the sum of their squares.
 */
 //--include file--//
-#include<stdio.h>
-#include<stdlib.h>
-//--Global Variables--//
-int main(){
-	int a;
-        int b;
-          printf("enter number 1");
-          scanf("%d",&a);
-          printf("enter number 2");
-          scanf("%d",&b);
-        int sum=(a*a)+(b*b);
-          printf("sum of two number: %d",sum);
+#include <stdio.h>
 
-	return 0;
+int main() {
+	int n,i;// n is any positive integer
+	int square_and_sum=0 ; 
+    printf("Enter the number (number=>0): ");
+    scanf("%d", &n);// in n store the integer 
+    int num[n];//stor more n numbers
+
+    for(i = 0; i < n; i++) {
+        printf("Enter integer %d: ", i + 1);
+        scanf("%d", &num[i]); 
+    }//end of loop i < n
+    for(i = 0; i < n; i++) {
+        square_and_sum=square_and_sum+num[i]*num[i];
+    }//end of 2nd loop
+
+    printf("The sum of squares is: %d\n", square_and_sum);
+
+    return 0;
 }//end main
