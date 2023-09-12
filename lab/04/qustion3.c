a104dc789e1b3834fe2aa29292fa7f0f719e81b1
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-12 10:48:45 +0500

    Create qustion3.c

---
+++ lab/04/qustion3.c
@@ -0,0 +1,20 @@
+/* programmer: talha rusman
+* Date: 12/9/2023
+* Related file: file of qustiom 3
+*/
+//--include file--//
+#include<stdio.h>
+#include<stdlib.h>
+//--Global Variables--//
+int main(){
+	int a;
+        int b;
+          printf("enter number 1");
+          scanf("%d",&a);
+          printf("enter number 2");
+          scanf("%d",&b);
+        int sum=(a*a)+(b*b);
+          printf("sum of two number: %d",sum);
+
+	return 0;
+}//end main
