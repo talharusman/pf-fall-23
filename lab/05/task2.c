fc695b51d99147a9f91dc59699dea4ba72e93a5f
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-19 09:18:32 +0500

    Create task2.c

---
+++ lab/05/task2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+           int n;
+          printf("enter any integer< 24:");
+          scanf("%d",&n);
+         if(n>=5&&n<=11){
+         printf("good morning");
+           }//n>=5&&n<=11
+          if(n>=12&&n<=18){
+          printf("good evening");
+          }//n>=12&&n<=18
+          if(n>18&n<=24){
+          printf("good night");
+          }//n>18&n<=24
+ return 0;
+
+}//end main
