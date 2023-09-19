5740c1058b77dbfbdf6afdf67992ba88653b3aec
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-19 09:16:57 +0500

    Create task1.c

---
+++ lab/05/task1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+           int n;
+          printf("enter any integer:");
+          scanf("%d",&n);
+         if(n%3==0){
+           printf("given number is muntipul of 3");
+           }else{
+           printf("given number is not muntipul of 3");
+           }
+ return 0;
+
+}//end main
