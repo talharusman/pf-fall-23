1bcfeb26732d7a3731cb4e5fc35184c3c968e1fe
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-19 09:56:49 +0500

    Create task4.c

---
+++ lab/05/task4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+           int i;	
+	    int j;
+              int n;
+           printf("enter 1st number:");
+           scanf("%d",&i);
+            printf("enter 2nd number:");
+           scanf("%d",&j);
+	    printf("enter any opperter:");
+           scanf("%d",&n);
+	   switch(n){
+  	  case'-': 
+            int sub=i-j;
+	   
+	   
+         
+ return 0;
+
+}//end main
