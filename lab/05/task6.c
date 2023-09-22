914a25c1784852cdc2a992515b3451b80019b68e
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-22 19:16:46 +0500

    Create task6.c

---
+++ lab/05/task6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+main(){
+      int n,i,j;
+      printf("entry 8 bit:");
+      scanf("%d", &n);
+      i= (n/1000) % 10;
+      j=(n/1000000) % 10;
+      if("i && j"){
+      n= n- 1000000 - 1000;
+      }
+      printf("%d", n);
+}// end to mai
