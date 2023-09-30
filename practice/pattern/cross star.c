a3fe05a15393eec0c4b01c971fbb47521efc58f9
talharusman <142867808+talharusman@users.noreply.github.com>
2023-09-30 23:36:48 +0500

    Add files via upload

---
+++ practice/pattern/cross star.c
@@ -0,0 +1,28 @@
+/*
+ enter number:3
+ * *
+  *
+ * *
+*/
+#include <stdio.h>
+
+int main(){
+	int i,j,a,n;
+	printf("enter number :");
+	scanf("%d",&n);
+	//printf("enter number of row:");
+	//	scanf("%d",&a);
+	
+	for (i=1;i<=n;i++){
+      for(j=1;j<=n;j++){
+      	if(i==j||i+j==n+1){
+      	  printf("*");
+        }else
+        printf(" ");
+	   }
+	 printf("\n");
+        }
+      
+		
+	return 0;
+}
