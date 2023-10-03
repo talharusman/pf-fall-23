f2ce3cb02117e2b17c6c79e6fea49c537e9f0ba4
talharusman <142867808+talharusman@users.noreply.github.com>
2023-10-03 19:06:02 +0500

    Create question07.c

---
+++ lab/06/question07.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main(){  
+    int i,j,a,b[100];
+    printf("enter how many numbers add:");
+    scanf("%d",&a);
+    int sum=0,k=1;
+    for(i=0;i<a;i++){
+    printf("enter numbers %d:",k);
+    scanf("%d",&b[i]);
+    }
+    k++;
+    for(i=a-1;i>=0;i--){
+     sum=sum+b[i];
+    }
+    printf ("sum is .%d",sum);
+
+}// end to main
