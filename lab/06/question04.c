2d0095d55caceaef9cd0f5200c6915746ec4a0a8
talharusman <142867808+talharusman@users.noreply.github.com>
2023-10-03 11:07:51 +0500

    Add files via upload

---
+++ lab/06/question04.c
@@ -0,0 +1,30 @@
+/*
+ * programer:talha rusman,23k-0065
+ *Desc:prac
+ *Date:3/10/2023
+*/
+#include<stdio.h>
+
+int main(){
+   int i,a,b;
+   printf("enter 2 number :");
+   scanf("%d %d",&a,&b);
+   for(i=a;i<=b;i++){
+       switch(i){
+         case 0:printf("Zero");break;
+         case 1:printf("One");break;
+         case 2:printf("Two");break;
+         case 3:printf("Three");break;
+	 case 4:printf("Four");break;
+	 case 5:printf("Five");break;
+	 case 6:printf("Six");break;
+	 case 7:printf("Seven");break;
+         case 8:printf("Eight");break;
+         case 9:printf("Nine");break;
+         default: if(i%2==0){
+          printf("even");}
+       else printf("odd");
+    }//end of switch statmint
+   
+ }//end for loop
+}//end main
