21ff2b40bd8face28b0cca5cdccf89286af28ee4
talharusman <142867808+talharusman@users.noreply.github.com>
2023-10-10 10:59:20 +0500

    Add files via upload

---
+++ lab/07/question07.c
@@ -0,0 +1,55 @@
+/*
+*programer: talha rusman 23k-0065
+*Dase: without divison oprater
+*Date:10/10/23
+*/
+#include <stdio.h>
+
+int main(){
+            int i,j,c,r;
+           int matrix1[3][3];
+            int matrix3[3][3];
+            int matrix3[3][3];
+            for(i=0;i<2;i++){ 
+                    for(j=0;j<2;j++){
+                    printf("entere ementment of (row%d,columb%d):",i+1,j+1);
+                    scanf("%d",&matrix2[i][j]);
+                 }
+           }
+               for(i=0;i<3;i++){ 
+                    for(j=0;j<2;j++){
+                    printf("%d ",matrix1[i][j]);
+                     if(j==1){
+                       printf("\n");
+                   }
+                 }
+           }
+                           for(i=0;i<2;i++){ 
+                             for(j=0;j<2;j++){
+                    printf("entere ementment of (row%d,columb%d):",i+1,j+1);
+                    scanf("%d",&matrix2[i][j]);
+                 }
+           }
+               for(i=0;i<2;i++){ 
+                    for(j=0;j<3;j++){
+                    printf("%d ",matrix2[i][j]);
+                     if(j==1){
+                       printf("\n");
+                   }
+                 }
+           }
+                    for(i=0;i<2;i++){
+                        for(j=0;j<2;j++){
+                    matrix[i][j]=
+                 }
+           }
+               for(i=0;i<2;i++){ 
+                    for(j=0;j<3;j++){
+                    printf("%d ",matrix[i][j]);
+                     if(j==1){
+                       printf("\n");
+                   }
+                 }
+           }
+        
+}
