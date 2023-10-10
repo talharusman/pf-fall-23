cc7f59c920662462a090691bc9fbaa66e11108bd
talharusman <142867808+talharusman@users.noreply.github.com>
2023-10-10 12:45:46 +0500

    Update question07.c

--- lab/07/question07.c
+++ lab/07/question07.c
@@ -8,43 +8,46 @@
 int main(){
             int i,j,c,r;
            int matrix1[3][3];
-            int matrix3[3][3];
-            int matrix3[3][3];
+            int matrix2[3][3];
+            int matrix[3][3];
             for(i=0;i<2;i++){ 
                     for(j=0;j<2;j++){
-                    printf("entere ementment of (row%d,columb%d):",i+1,j+1);
-                    scanf("%d",&matrix2[i][j]);
+                    printf("entere ementment of matrix1 (i%d,j%d):",i+1,j+1);
+                    scanf("%d",&matrix1[i][j]);
                  }
            }
-               for(i=0;i<3;i++){ 
+           
+                           for(i=0;i<2;i++){ 
+                             for(j=0;j<2;j++){
+                    printf("entere ementment of matrox2(i%d,j%d):",i+1,j+1);
+                    scanf("%d",&matrix2[i][j]);
+                   }
+                }
+                  printf("matrix 1\n");
+               for(i=0;i<2;i++){ 
                     for(j=0;j<2;j++){
                     printf("%d ",matrix1[i][j]);
                      if(j==1){
                        printf("\n");
                    }
                  }
-           }
-                           for(i=0;i<2;i++){ 
-                             for(j=0;j<2;j++){
-                    printf("entere ementment of (row%d,columb%d):",i+1,j+1);
-                    scanf("%d",&matrix2[i][j]);
-                 }
-           }
+                }
+                 printf("matrix 2\n");
                for(i=0;i<2;i++){ 
-                    for(j=0;j<3;j++){
+                    for(j=0;j<2;j++){
                     printf("%d ",matrix2[i][j]);
                      if(j==1){
                        printf("\n");
-                   }
-                 }
-           }
-                    for(i=0;i<2;i++){
-                        for(j=0;j<2;j++){
-                    matrix[i][j]=
-                 }
-           }
+                      }
+                    }
+                }
+                for(i=0;i<2;i++){
+                    for(j=0;j<2;j++){
+                    matrix[i][j]=matrix1[i][j]*matrix2[i][j]
+                    }
+                }
                for(i=0;i<2;i++){ 
-                    for(j=0;j<3;j++){
+                    for(j=0;j<2;j++){
                     printf("%d ",matrix[i][j]);
                      if(j==1){
                        printf("\n");
