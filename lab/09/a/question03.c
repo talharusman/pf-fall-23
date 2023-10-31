1e135d08d2e8fcc7193bd2fe686e18bc78919497
talharusman <142867808+talharusman@users.noreply.github.com>
2023-10-31 19:34:34 +0500

    Update question03.c

--- lab/09/a/question03.c
+++ lab/09/a/question03.c
@@ -1,33 +1,56 @@
+/*
+*programmer:talha rusman
+*Dase:using pointer print the value of array
+*Date:31/10/23
+*/
 #include <stdio.h>
-void swap(int *k,int){
 
-}
+int main() {
+    int arr1[1000];
+    char arr2[1000];
+    long long int arr3[1000];
+    int n;
+    int i;
+    printf("enter the size of array: ");
+    scanf("%d", &n);
 
-int main(){
-	int arr1[1000];
-	char arr2[1000];
-	long long int arr3[1000];
-	int n;
-	int i;
-	printf("enter the size of array\n");
-	scanf("%d",&n);
-	
-	printf("enter int type array's element\n");
-	for (i = 0; i < n;i++){
-	    scanf("%d"&arr1[i]);
-	}
-	printf("enter char type array's element\n");
-	for (i = 0; i < n;i++){
-	    scanf("%s"&arr2[i]);
-	}	
-	printf("enter long long int type array's element");
-	for (i = 0; i < n;i++){
-	    scanf("%lli"&arr3[i]);
-	}
-	int *a=arr1[0];
-	int *b=arr2[0];
-	int *c=arr3[0];
-	printf("");
+    printf("enter int type array's elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr1[i]);
+    }
 
-	return 0;
+    printf("enter char type array's elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf(" %c", &arr2[i]); // Notice the space before %c to consume the newline character.
+
+    }
+
+    printf("enter long long int type array's elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%lld", &arr3[i]);
+    }
+
+    int *pnt1 = &arr1[0];
+    char *pnt2 = &arr2[0];
+    long long int *pnt3 = &arr3[0];
+
+    printf("for int type array's:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Value of arr1[%d] is %d, Address is %p\n", i, *pnt1, (void *) pnt1);
+        pnt1++;
+    }
+
+    printf("for char type array's:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Value of arr2[%d] is %c, Address is %p\n", i, *pnt2, (void *) pnt2);
+        pnt2++;
+    }
+
+    printf("for long long int type array's:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Value of arr3[%d] is %lld, Address is %p\n", i, *pnt3, (void *) pnt3);
+        pnt3++;
+    }
+
+    return 0;
 }
